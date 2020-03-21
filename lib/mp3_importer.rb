8b079f591014c711b4adc0056f197117c166f1b2
taroj khan <tarujkhan@yahoo.com>
2020-03-21 19:23:09 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,14 +7,7 @@ class mp3_importer
   end
   
   def files 
-    files = []
-    files = Dir.entries(@path)
-    file.each do |file|
-      if file.include?("mp3")
-        @files << file 
-  end 
-end 
-  @files 
+  
 end 
 
 def import 
