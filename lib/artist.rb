5c47b83c00a89acadcc7ab738fe83ebf6285012b
taroj khan <tarujkhan@yahoo.com>
2020-03-21 19:05:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,6 +8,10 @@ class Artist
     @@all << self 
   end
   
+   def self.all 
+    @@all 
+  end 
+  
   def songs
     Song.all.select { | song | song.artist == self}
   end 
@@ -21,11 +25,6 @@ class Artist
     @@all << self 
     self
   end
-  
-  
-  def self.all 
-    @@all 
-  end 
   
   def self.find_or_create_by_name(name)
     self.all.find {|artist| artist.name == name} || self.name(name).save
