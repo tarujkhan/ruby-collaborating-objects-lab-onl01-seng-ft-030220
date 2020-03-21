530101a9c4e2bde2f91af26e3c7b5b26f76887a7
taroj khan <tarujkhan@yahoo.com>
2020-03-21 19:03:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,9 +9,9 @@ class Artist
   end
   
   def songs
-    Song.all.select do { | song | song.artist == self}
+    Song.all.select { | song | song.artist == self}
   end 
-end   
+
 
   def add_song(song)
     song.artist = self 
