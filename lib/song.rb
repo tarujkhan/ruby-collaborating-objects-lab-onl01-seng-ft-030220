4ab4f73bb0f62b7bc7c3912652f999bd1f8d1473
taroj khan <tarujkhan@yahoo.com>
2020-03-21 19:15:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,14 +20,7 @@ end
   
   
   def self.new_by_filename(file)
-    song_names = file.split("-")[1]
-    artist = file.split("-")[0]
-    song = self.new(song_name)
-    song.artist_name = artist 
-    song 
+   Artist, 
   end 
   
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-  self.artist.add_song(self)
-end 
+  
